80ae2fec8cc8e93c0542034684435de102169194
phuongthuy2803 <nguyenngocphuongthuy2803@gmail.com>
2024-08-25 18:16:44 +0700

    Delete bai 5 slide 19.py

--- bai 5 slide 19.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 25 08:30:13 2024
-
-@author: Student
-"""
-
-t= int(input("Năm sinh của bạn: "))
-print("Bạn sinh năm ", t, "vậy bạn ", 2024-t, "tuổi")
