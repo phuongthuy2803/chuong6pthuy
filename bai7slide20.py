0edc1c570bb9294cbb1c7f99d2d4efa2c96864c3
phuongthuy2803 <nguyenngocphuongthuy2803@gmail.com>
2024-08-25 12:04:04 +0700

    Add files via upload

---
+++ bai7slide20.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 08:53:27 2024
+
+@author: Student
+"""
+
+print("============ MENU ============ \n")
+print("1. Hu tieu \n2. Chao long \n3. Banh canh \n4. Bun rieu \n5. Pho bo \n")
+print("============================== \n ")
+print("Moi nhap lua chon: \n ")
+print("==============================")
